4d4de7b8d5464da7ccb539a06bf4dba60cac1759
Kristof Zerbe <kristof.zerbe@gmail.com>
2021-04-03 15:41:30 +0200

    Layout

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+if (
+    "IntersectionObserver" in window &&
+    "IntersectionObserverEntry" in window &&
+    "intersectionRatio" in window.IntersectionObserverEntry.prototype
+) {
+    let observer = new IntersectionObserver((entries) => {
+        if (entries[0].boundingClientRect.y < 0) {
+            document.body.classList.add("scrolled");
+        } else {
+        document.body.classList.remove("scrolled");
+        }
+    });
+    observer.observe(document.querySelector("#scroll-anchor"));
+}

60d8e321165222590d0aa7348d340e11b2bb0543
Kristof Zerbe <kristof.zerbe@gmail.com>
2021-04-05 12:17:43 +0200

    Layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
-if (
-    "IntersectionObserver" in window &&
-    "IntersectionObserverEntry" in window &&
-    "intersectionRatio" in window.IntersectionObserverEntry.prototype
-) {
-    let observer = new IntersectionObserver((entries) => {
-        if (entries[0].boundingClientRect.y < 0) {
-            document.body.classList.add("scrolled");
-        } else {
-        document.body.classList.remove("scrolled");
-        }
-    });
-    observer.observe(document.querySelector("#scroll-anchor"));
+
+function throttle(fn, wait) {
+	var time = Date.now();
+  	return function(e) {      
+		if ((time + wait - Date.now()) < 0) {
+			fn(e);
+			time = Date.now();
+		}
+	}
 }
+function scrollDetect() {
+	let scrollY = window.scrollY;
+	window.addEventListener('scroll', throttle(handleScroll,10));
+  
+	function handleScroll(e) {  
+		if(window.scrollY > scrollY) {
+			document.body.setAttribute("data-scroll", "DOWN");
+		} else if (window.scrollY < scrollY){
+			document.body.setAttribute("data-scroll", "UP");
+		}
+		if (window.scrollY === 0){
+			document.body.removeAttribute("data-scroll"); 
+		}    
+		scrollY = window.scrollY;
+	}
+}
+scrollDetect();
